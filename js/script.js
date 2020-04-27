cff52c295b80c01596c31662156fc239af415705
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2020-04-27 16:48:45 -0300

    criando arquivo js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+let bt = document.getElementById('bt');
+bt.addEventListener("click",buscarFunc);
+
+function buscarFunc(){
+    let input = document.getElementById('buscar').value;
+    alert(input);
+    console.log(input);
+}
+
+
